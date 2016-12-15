9dd3c9c19ecb9ada44e7ddb61442729369a79416
daniel kafer <daniel.j.kafer@gmail.com>
2016-12-14 18:41:01 -0600

    changed bcrypt back to bcryptjs

--- models/user_model.js
+++ models/user_model.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt');
+var bcrypt = require('bcryptjs');
 var UserSchema = new mongoose.Schema({
     username: {
         type: String,
