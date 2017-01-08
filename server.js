42f3728f9fc5ef955a5329d835db263a6caee311
daniel kafer <daniel.j.kafer@gmail.com>
2017-01-08 16:43:58 -0600

    changed login response to be json

--- server.js
+++ server.js
@@ -167,7 +167,7 @@ app.post('/users', jsonParser, function(req, res) {
 
 //Login
 app.post('/login', Authenticate, function(req, res) {
-	res.send(200).json("Login success");
+	res.send(200)({message: "Login Success"});
 });
 
 
