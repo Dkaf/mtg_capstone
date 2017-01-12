fa247511fd7bf361f19a2388ef992bd993c47570
daniel kafer <daniel.j.kafer@gmail.com>
2017-01-11 18:28:53 -0600

    added populate on card list update

--- server.js
+++ server.js
@@ -344,7 +344,9 @@ app.get('/deck/:deckSearch' ,function(req, res) {
 //Add cards to deck
 app.put('/user/deck/:deckName', Authenticate, function(req, res) {
     var deckName = req.params.deckName;
-    Deck.findOneAndUpdate({name: deckName}, {cards: req.body.cards}, function(err, deck) {
+    Deck.findOneAndUpdate({name: deckName}, {cards: req.body.cards}),
+	.populate('cards')
+	.exec(function(err, deck) {
         if (err) {
             return res.status(500).json({
                 message: 'Internal server error'
