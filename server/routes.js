09f2a9c639dde0acf472921d2a33ce55e110bf5d
daniel kafer <daniel.j.kafer@gmail.com>
2017-07-10 16:43:26 -0500

    Added user endpoints from server.js

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,17 @@
 import express from 'express';
 
+// Controllers
+import userController from './controllers/userController';
+
 const routes = express();
+
+// User Routes
+routes.post('/users', userController.newUser);
+
+// Import Authenticate
+
+routes.post('/login', Authenticate, userController.login);
+
+
 
 export default routes
