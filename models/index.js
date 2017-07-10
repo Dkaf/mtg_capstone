13ece126401a0bab2b052da88d0f2622e82699c4
daniel kafer <daniel.j.kafer@gmail.com>
2017-07-10 16:43:13 -0500

    Removed user endpoints

---
+++ models/index.js
@@ -0,0 +1,11 @@
+import Cache from './cache_model';
+import Card from './card_model';
+import Deck from './deck_model';
+import User from './user_model';
+
+export default {
+	Cache,
+	Card,
+	Deck,
+	User
+};
