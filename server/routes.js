f3b8ccd6c37727e5dd65f1a240001b58030cfda9
daniel kafer <daniel.j.kafer@gmail.com>
2017-07-14 18:04:14 -0500

    Added Deck endpoints

--- server/routes.js
+++ server/routes.js
@@ -3,6 +3,7 @@ import express from 'express';
 // Controllers
 import userController from './controllers/userController';
 import cardController from './controllers/cardController';
+import deckController from './controllers/deckController';
 
 const routes = express();
 
@@ -15,6 +16,7 @@ routes.post('/login', Authenticate, userController.login);
 
 
 // Card Routes
+
 routes.get('/cards/all', cardController.allCards);
 
 routes.get('/cards/:id', cardController.cardId);
@@ -22,6 +24,18 @@ routes.get('/cards/:id', cardController.cardId);
 routes.get('/cards/', cardContoller.filteredCards);
 
 routes.post('/cards/', cardController.cacheCards);
+
+// Deck Routes
+
+routes.post('/user/deck', Authenticate, deckController.newDeck);
+
+routes.get('/decks/:user', deckController.getDecks);
+
+routes.get('/deck/:deckSearch', deckController.findDeck);
+
+routes.get('/user/deck/:deckName', Authenticate, deckController.addCards);
+
+routes.delete('/user/deck/:deckName', deckController.deleteDeck);
 
 
 export default routes
